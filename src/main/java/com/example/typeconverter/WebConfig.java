168aa1b2a5d093327d0d63c3908fe1fd4c55afb5
minu <made_power@naver.com>
2023-04-30 16:49:03 +0900

    포맷터 적용
    컨트롤러에 포맷터를 적용시키고 잘 동작하는지 확인

--- src/main/java/com/example/typeconverter/WebConfig.java
+++ src/main/java/com/example/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import com.example.typeconverter.converter.IntegerToStringConverter;
 import com.example.typeconverter.converter.IpPortToStringConverter;
 import com.example.typeconverter.converter.StringToInteger;
 import com.example.typeconverter.converter.StringToIpPortConverter;
+import com.example.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,9 +12,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToInteger());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToInteger());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
